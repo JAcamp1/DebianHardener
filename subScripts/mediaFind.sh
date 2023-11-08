9273ba76de20f834fa72759caa30837502ffe467
WallRunner <47185839+JAcamp1@users.noreply.github.com>
2023-11-08 18:33:35 -0500

    Create mediaFind.sh
    Finds all media objects, .jpg, .jpeg, .png, .gif are only searched on home directories

---
+++ subScripts/mediaFind.sh
@@ -0,0 +1,16 @@
+#Script for finding all types of media considered "hacker tools"
+
+sudo find / -name '*.mp3' -type f
+sudo find / -name '*.mov' -type f
+sudo find / -name '*.mp4' -type f
+sudo find / -name '*.avi' -type f
+sudo find / -name '*.mpg' -type f
+sudo find / -name '*.mpeg' -type f
+sudo find / -name '*.flac' -type f
+sudo find / -name '*.m4a' -type f
+sudo find / -name '*.flv' -type f
+sudo find / -name '*.ogg' -type f
+sudo find /home -name '*.gif' -type f
+sudo find /home -name '*.png' -type f
+sudo find /home -name '*.jpg' -type f
+sudo find /home -name '*.jpeg' -type f
