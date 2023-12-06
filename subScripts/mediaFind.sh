3c7ef088d59631bed18dae66bcd3cd4064c02200
WallRunner <47185839+JAcamp1@users.noreply.github.com>
2023-12-06 17:35:04 -0500

    Update mediaFind.sh
    Changed to echo each file.

--- subScripts/mediaFind.sh
+++ subScripts/mediaFind.sh
@@ -1,16 +1,16 @@
 #Script for finding all types of media considered "hacker tools"
 
-sudo find / -name '*.mp3' -type f
-sudo find / -name '*.mov' -type f
-sudo find / -name '*.mp4' -type f
-sudo find / -name '*.avi' -type f
-sudo find / -name '*.mpg' -type f
-sudo find / -name '*.mpeg' -type f
-sudo find / -name '*.flac' -type f
-sudo find / -name '*.m4a' -type f
-sudo find / -name '*.flv' -type f
-sudo find / -name '*.ogg' -type f
-sudo find /home -name '*.gif' -type f
-sudo find /home -name '*.png' -type f
-sudo find /home -name '*.jpg' -type f
-sudo find /home -name '*.jpeg' -type f
+echo '$(sudo find / -name '*.mp3' -type f)'
+echo '$(sudo find / -name '*.mov' -type f)'
+echo '$(sudo find / -name '*.mp4' -type f)'
+echo '$(sudo find / -name '*.avi' -type f)'
+echo '$(sudo find / -name '*.mpg' -type f)'
+echo '$(sudo find / -name '*.mpeg' -type f)'
+echo '$(sudo find / -name '*.flac' -type f)'
+echo '$(sudo find / -name '*.m4a' -type f)'
+echo '$(sudo find / -name '*.flv' -type f)'
+echo '$(sudo find / -name '*.ogg' -type f)'
+echo '$(sudo find /home -name '*.gif' -type f)'
+echo '$(sudo find /home -name '*.png' -type f)'
+echo '$(sudo find /home -name '*.jpg' -type f)'
+echo '$(sudo find /home -name '*.jpeg' -type f)'
