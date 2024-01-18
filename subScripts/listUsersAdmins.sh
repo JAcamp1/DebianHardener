91dcd8b96309053b9942a1624216c5ad7a4d8b03
WallRunner <47185839+JAcamp1@users.noreply.github.com>
2024-01-18 16:12:10 -0500

    User audit overhaul
    now prompts options for each user.

--- subScripts/listUsersAdmins.sh
+++ subScripts/listUsersAdmins.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 #Gets list of users and admins, prints list and prompts removal.
-
-sudoers=$(grep -i --color 'sudo' /etc/group)
-sudoers=${sudoers##sudo:x:*:}
-sudoers=${sudoers//,/'\n'}
-
-echo Admins...
-echo
-printf ${sudoers}
-echo
-echo
-
 users="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd)"
 users=${users#:*}
 
-echo Users...
-echo
-printf "${users}"
-echo
-echo
+array=()
+for i in $users; do array+=($i); done
+for i in ${users[@]}
+do
+	echo "$i"
+	read -p "What kind of user should this be? (s, r, n) ... " usertype
+	echo "$usertype"
+	if [ $usertype = 's' ]
+	then 
+		sudo usermod -g $i $i
+		sudo usermod -a -G sudo $i
+		echo "user now in sudoers"
+	fi
+	if [ $usertype = 'r' ]
+	then
+		sudo usermod -g $i $i
+		echo "user now standard"
+	fi
+	if [ $usertype = 'n' ]
+	then 
+		sudo userdel $i
+		echo "user removed"
+	fi
+done
